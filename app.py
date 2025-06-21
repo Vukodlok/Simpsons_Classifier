9d8d90f708e29cbee0557a04bf2b1c6c58b68c3e
Vukodlok <52221871+Vukodlok@users.noreply.github.com>
2025-06-21 18:42:57 -0400

    Update clear the results when the clear button is clicked

--- app.py
+++ app.py
@@ -113,8 +113,8 @@ with gr.Blocks(css=custom_css) as demo:
     )
 
     clear_btn.click(
-        lambda: (None, gr.update(value="", visible=False)),
-        outputs=[image_input, result_message]
+        lambda: (None, {}, gr.update(value="", visible=False)),
+        outputs=[image_input, output, result_message]
     )
 
 demo.launch(share=True, server_name="0.0.0.0", server_port=7860)
